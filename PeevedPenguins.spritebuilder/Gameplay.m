a2bd9a3711e9b70b7a53b1216bdccfc9af75e1ec
Robert von Stange <rvonstan@andrew.cmu.edu>
2015-01-27 17:40:33 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -12,6 +12,7 @@
 
 CCPhysicsNode *_physicsNode;
 CCNode *_catapultArm;
+CCNode *_levelNode;
 
 }
 
@@ -19,6 +20,8 @@ CCNode *_catapultArm;
 - (void)didLoadFromCCB {
     // tell this scene to accept touches
     self.userInteractionEnabled = TRUE;
+    CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
+    [_levelNode addChild:level];
 }
 
 // called on every touch in this scene
