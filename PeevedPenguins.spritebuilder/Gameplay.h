bd158c353aa8522c0a2e0cb561501cbe3f4037e3
Robert von Stange <rvonstan@andrew.cmu.edu>
2015-01-27 15:21:05 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Robert von Stange on 1/27/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Gameplay : CCSprite
+
+@end
