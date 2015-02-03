2cfeca4a2fc88e327fe99fa1a7b5369097e5c569
Robert von Stange <rvonstan@andrew.cmu.edu>
2015-02-03 01:59:18 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -146,7 +146,7 @@ CCPhysicsJoint *_penguinCatapultJoint;
     
     // manually create & apply a force to launch the penguin
     CGPoint launchDirection = ccp(1, 0);
-    CGPoint force = ccpMult(launchDirection, 5000);
+    CGPoint force = ccpMult(launchDirection, 3500);
     [penguin.physicsBody applyForce:force];
     
     // ensure followed object is in visible are when starting
