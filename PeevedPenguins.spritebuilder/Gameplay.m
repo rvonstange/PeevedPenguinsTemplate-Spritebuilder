e96db176deb0c0516b4836aa05473d9530db52d1
Robert von Stange <rvonstan@andrew.cmu.edu>
2015-02-03 20:22:05 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -43,9 +43,10 @@ CCPhysicsJoint *_penguinCatapultJoint;
         _currentPenguin.physicsBody.allowsRotation = TRUE;
         
         // follow the flying penguin
-        self.position = ccp(0, 0);        
+        self.position = ccp(0, 0);
         CCActionFollow *follow = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
         [_contentNode runAction:follow];
+        _contentNode.position = ccp(0,0);
     }
 }
 
