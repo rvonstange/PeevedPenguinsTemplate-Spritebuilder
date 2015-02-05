f488816f79906ce2d92ae9b6c57ff1191fde1b1a
Robert von Stange <rvonstan@andrew.cmu.edu>
2015-02-05 15:50:23 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Robert von Stange on 2/5/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/WaitingPenguin.m
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.m
+//  PeevedPenguins
+//
+//  Created by Robert von Stange on 2/5/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "WaitingPenguin.h"
+
+@implementation WaitingPenguin
+
+@end
